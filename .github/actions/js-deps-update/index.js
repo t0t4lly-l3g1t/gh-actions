16b647d2b1408e69b77b153f213282ecbdeca55a
t0t4lly-l3g1t <chris@naughtron.com>
2024-07-31 16:33:25 -0400

    more JS added to the index.js that is used by the js custom action

--- .github/actions/js-deps-update/index.js
+++ .github/actions/js-deps-update/index.js
@@ -1,7 +1,58 @@
 const core = require('@actions/core');
+const exec = require('@actions/exec');
+
+// Matches alphanumeric string with _-./; no spaces or other chars allowed
+const validateBranchName = ({ branchName }) => /^[a-zA-Z0-9_\-\.\/]+$/.test(branchName);
+const validateDirectoryhName = ({ dirName }) => /^[a-zA-Z0-9_\-\/]+$/.test(dirName);
 
 async function run() {
-    /*
+    const baseBranch = core.getInput('base-branch');
+    const targetBranch = core.getInput('target-branch');
+    const ghToken = core.getInput('gh-token'); // this needs to be a secret
+    const workingDir = core.getInput('working-directory');
+    const debug = core.getBooleanInput('debug');
+
+    core.setSecret('ghToken'); // protect the GitHub token by making it a secret. 
+
+    if (!validateBranchName({ branchName: baseBranch })) {
+        core.setFailed('Invalid base branch name. Branch names should only include chars, numbers, hyphens, underscores, dots, and forward slashes')
+        return;
+    }
+
+    if (!validateBranchName({ branchName: targetBranch })) {
+        core.setFailed('Invalid target branch name. Branch names should only include chars, numbers, hyphens, underscores, dots, and forward slashes')
+        return;
+    }
+
+    if (!validateDirectoryhName({ dirName: workingDir })) {
+        core.setFailed('Invalid working directory name. Branch names should only include chars, numbers, hyphens, underscores, and forward slashes')
+        return;
+    }
+
+    core.info(`[js-dependency-update] : base branch is ${baseBranch}`);
+    core.info(`[js-dependency-update] : traget branch is ${targetBranch}`);
+    core.info(`[js-dependency-update] : working directory is ${workingDir}`);
+
+    await exec.exec('npm update', [], {
+        cwd: workingDir
+    });
+
+    const gitStatus = await exec.getExecOutput('git status -s package*.json', [], {
+        cwd: workingDir
+    });
+
+    if (gitStatus.stdout.length > 0) {
+        core.info('[js-dependency-update] : There are updates available.')
+    } else {
+        core.info('[js-dependency-update] : No updates at this time.')
+    }
+
+    core.info('I am a custom JS action.');
+}
+
+run();
+
+  /*
     1. parse inputs: 
      1.1 base-branch from which to check for updates. 
      1.2 target-branch to use to create the PR.
@@ -14,7 +65,3 @@ async function run() {
      4.2 Create a PR to the base-branch using the target-branch using the octokit API
     5. Otherwise, conclude the custom action. 
     */
-    core.info('I am a custom JS action.');
-}
-
-run();
