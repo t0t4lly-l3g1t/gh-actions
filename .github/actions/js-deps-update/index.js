4381b30f235a6ac1b198ee8ab5d03437bfd1c888
t0t4lly-l3g1t <chris@naughtron.com>
2024-08-01 11:22:01 -0400

    fix and added GITHUB_TOKEN to workflwo

--- .github/actions/js-deps-update/index.js
+++ .github/actions/js-deps-update/index.js
@@ -1,18 +1,23 @@
 const core = require('@actions/core');
 const exec = require('@actions/exec');
+const github = require('@actions/github');
 
 // Matches alphanumeric string with _-./; no spaces or other chars allowed
 const validateBranchName = ({ branchName }) => /^[a-zA-Z0-9_\-\.\/]+$/.test(branchName);
 const validateDirectoryName = ({ dirName }) => /^[a-zA-Z0-9_\-\/]+$/.test(dirName);
 
 async function run() {
-    const baseBranch = core.getInput('base-branch');
-    const targetBranch = core.getInput('target-branch');
-    const ghToken = core.getInput('gh-token'); // this needs to be a secret
-    const workingDir = core.getInput('working-directory');
+    const baseBranch = core.getInput('base-branch', { required: true });
+    const targetBranch = core.getInput('target-branch', { required: true });
+    const ghToken = core.getInput('gh-token', { required: true });
+    const workingDir = core.getInput('working-directory', { required: true });
     const debug = core.getBooleanInput('debug');
     
-    core.setSecret('ghToken'); // protect the GitHub token by making it a secret. 
+    const commonExecOpts = {
+        cwd: workingDir
+    };
+    
+    core.setSecret(ghToken); // protect the GitHub token by making it a secret. 
     
     if (!validateBranchName({ branchName: baseBranch })) {
         core.setFailed('Invalid base branch name. Branch names should only include chars, numbers, hyphens, underscores, dots, and forward slashes');
@@ -33,21 +38,45 @@ async function run() {
     core.info(`[js-dependency-update] : target branch is ${targetBranch}`);
     core.info(`[js-dependency-update] : working directory is ${workingDir}`);
     
-    await exec.exec('npm update', [], {
-        cwd: workingDir
-    });
+    await exec.exec('npm update', [], commonExecOpts);
     
-    const gitStatus = await exec.getExecOutput('git status -s package*.json', [], {
-        cwd: workingDir
-    });
+    const gitStatus = await exec.getExecOutput('git status -s package*.json', [], commonExecOpts);
     
     if (gitStatus.stdout.length > 0) {
         core.info('[js-dependency-update] : There are updates available.');
+        await exec.exec('git config --global user.name "gh-automation"');
+        await exec.exec('git config --global user.email "gh-automation@email.com"');
+        await exec.exec(`git checkout -b ${targetBranch}`, [], commonExecOpts);
+        await exec.exec('git add package.json package-lock.json', [], commonExecOpts);
+        await exec.exec('git commit -m "chore: update dependencies"', [], commonExecOpts);
+        
+        try {
+            await exec.exec(`git push -u origin ${targetBranch}`, [], commonExecOpts);
+        } catch (error) {
+            core.warning('Failed to push branch. Attempting to rebase and push...');
+            await exec.exec(`git pull --rebase origin ${baseBranch}`, [], commonExecOpts);
+            await exec.exec(`git push -u origin ${targetBranch}`, [], commonExecOpts);
+        }
+
+        const octokit = github.getOctokit(ghToken);
+        try {
+            await octokit.rest.pulls.create({
+                owner: github.context.repo.owner,
+                repo: github.context.repo.repo,
+                title: 'Update NPM dependencies',
+                body: 'This PR updates NPM packages',
+                base: baseBranch,
+                head: targetBranch
+            });
+        } catch (e) {
+            core.error('[js-dependency-update] : Something went wrong creating the PR. Check logs below.');
+            core.setFailed(e.message);
+            core.error(e);
+        }
+        
     } else {
         core.info('[js-dependency-update] : No updates at this time.');
-    }
-    
-    core.info('I am a custom JS action.');
+    }    
 }
 
 run();
