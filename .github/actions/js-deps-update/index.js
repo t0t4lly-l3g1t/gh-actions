a56a79157bef171054f942681b720f9f8f11f58c
t0t4lly-l3g1t <chris@naughtron.com>
2024-08-01 14:45:23 -0400

    more refactoring and fixzes

--- .github/actions/js-deps-update/index.js
+++ .github/actions/js-deps-update/index.js
@@ -11,19 +11,36 @@ const setupGit = async () => {
 const validateBranchName = ({ branchName }) => /^[a-zA-Z0-9_\-\.\/]+$/.test(branchName);
 const validateDirectoryName = ({ dirName }) => /^[a-zA-Z0-9_\-\/]+$/.test(dirName);
 
+const setupLogger = ({debug, prefix} = {debug: false, prefix: ''}) => ({
+    debug: (message) => {
+        if (debug){
+            core.info(`DEBUG ${prefix}${prefix ? ' : ' : ''}${message}`);
+            // this allows for extending the logging functionality
+        }
+    },
+    info: (message) => {
+        core.info(`${prefix}${prefix ? ' : ' : ''}${message}`);
+    },
+    error: (message) => {
+        core.error(`${prefix}${prefix ? ' : ' : ''}${message}`);
+    }
+});
+
 async function run() {
     const baseBranch = core.getInput('base-branch', { required: true });
     const headBranch = core.getInput('head-branch', { required: true });
     const ghToken = core.getInput('gh-token', { required: true });
     const workingDir = core.getInput('working-directory', { required: true });
     const debug = core.getBooleanInput('debug');
+    const logger = setupLogger({debug, prefix: '[JS-dependency-update]'});
     
     const commonExecOpts = {
         cwd: workingDir
     };
     
     core.setSecret(ghToken); // protect the GitHub token by making it a secret. 
-    
+    logger.debug('Validating Inputs - base-branch, head-branch, working-directory');
+
     if (!validateBranchName({ branchName: baseBranch })) {
         core.setFailed('Invalid base branch name. Branch names should only include chars, numbers, hyphens, underscores, dots, and forward slashes');
         return;
@@ -39,17 +56,23 @@ async function run() {
         return;
     }
     
-    core.info(`[js-dependency-update] : base branch is ${baseBranch}`);
-    core.info(`[js-dependency-update] : head branch is ${headBranch}`);
-    core.info(`[js-dependency-update] : working directory is ${workingDir}`);
+    logger.debug(`base branch is ${baseBranch}`);
+    logger.debug(`head branch is ${headBranch}`);
+    logger.debug(`working directory is ${workingDir}`);
     
+    logger.debug('Checking for package updates');
+
     await exec.exec('npm update', [], commonExecOpts);
     
     const gitStatus = await exec.getExecOutput('git status -s package*.json', [], commonExecOpts);
     
     if (gitStatus.stdout.length > 0) {
-        core.info('[js-dependency-update] : There are updates available.');
+        logger.debug('There are updates available.');
+
+        logger.debug('Setting up git');
         await setupGit();
+        logger.debug('Committing and pushing package.json changes');
+
         await exec.exec(`git checkout -b ${headBranch}`, [], commonExecOpts);
         await exec.exec('git add package.json package-lock.json', [], commonExecOpts);
         await exec.exec('git commit -m "chore: update dependencies"', [], commonExecOpts);
@@ -61,9 +84,10 @@ async function run() {
             await exec.exec(`git pull --rebase origin ${baseBranch}`, [], commonExecOpts);
             await exec.exec(`git push -u origin ${headBranch}`, [], commonExecOpts);
         }
-
+        logger.debug('Fetching Octokit API');
         const octokit = github.getOctokit(ghToken);
         try {
+            logger.debug(`Creating PR using head-branch ${headBranch}`);
             await octokit.rest.pulls.create({
                 owner: github.context.repo.owner,
                 repo: github.context.repo.repo,
@@ -73,13 +97,13 @@ async function run() {
                 head: headBranch
             });
         } catch (e) {
-            core.error('[js-dependency-update] : Something went wrong creating the PR. Check logs below.');
+            logger.error('Something went wrong creating the PR. Check logs below.');
             core.setFailed(e.message);
-            core.error(e);
+            logger.error(e);
         }
         
     } else {
-        core.info('[js-dependency-update] : No updates at this time.');
+        logger.info('No updates at this time.');
     }    
 }
 
